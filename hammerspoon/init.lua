347cdea5703caf61e57e3050353c1e6146fa784c
Nathan Schott <nschott3@gmail.com>
2022-11-03 22:50:11 -0400

    Improve marks for minimized and hidden windows

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -11,11 +11,29 @@ function printTable(table)
     end
 end
 
-local filter = hs.window.filter.new(true)
+local function getAllWindows()
+    -- Get all visible windows across spaces, including fullscreen
+    -- hs.window.filter has trouble getting minimized and hidden windows
+    local visibleWindows = hs.window.filter.new():getWindows()
+    -- Standard hs.window functions can't grab windows across spaces
+    local minimizedWindows = hs.window.minimizedWindows()
+    local invisibleWindows = hs.window.invisibleWindows()
 
-printTable(filter:getFilters())
-printTable(filter:getWindows())
+    print("--- ALL WINDOWS ---")
+    printTable(visibleWindows)
+    print("--- MINIMIZED WINDOWS ---")
+    printTable(minimizedWindows)
+    print("--- INVISIBLE WINDOWS ---")
+    printTable(invisibleWindows)
 
+    local allWindows = {}
+
+    for _, window in ipairs(visibleWindows) do table.insert(allWindows, window) end
+    for _, window in ipairs(minimizedWindows) do table.insert(allWindows, window) end
+    for _, window in ipairs(invisibleWindows) do table.insert(allWindows, window) end
+
+    return allWindows
+end
 
 local function getDeserializedMarks()
     local status, serializedMarks = pcall(function() return hs.json.read(MARKS_FILE_PATH) end)
@@ -24,18 +42,20 @@ local function getDeserializedMarks()
         return {}
     end
 
+    local allWindows = getAllWindows()
     local deserializedMarks = {}
 
     for k, v in pairs(serializedMarks) do
-        local filter = hs.window.filter.new(function(win)
-            -- print(window)
-            return win:id() == v.window
-        end):setDefaultFilter {}
+        local window = nil
 
-        local window = filter:getWindows()[1]
+        for _, win in ipairs(allWindows) do
+            if win:id() == v.window then
+                window = win
+                break
+            end
+        end
+
         local application = hs.application.get(v.application) or v.application
-
-        -- print(k, window)
 
         deserializedMarks[k] = {
             window = window,
@@ -62,25 +82,38 @@ local function getSerializedMarks()
 end
 
 local function showApplication(application, window, key)
-    print("window", window)
-    if window and window:application():bundleID() == application:bundleID() then
-        if window:isVisible() then
-            window:focus()
-        else
-            window:maximize()
+    print("--- showApplication ---")
+    print("key ", key)
+    print("application ", application)
+    print("window ", window)
+
+    if not application then return end
+
+    if next(application:allWindows()) == nil then
+        -- A closed window can be held onto so check to see if all application windows are closed
+
+        hs.application.open(application:bundleID(), 0, true)
+
+    elseif window and window:application():bundleID() == application:bundleID() then
+        -- Check to ensure that the window ID that may have been deserialized back to a window
+        -- instance wtill matches the same application
+
+        if window:isMinimized() then
+            window:unminimize()
         end
+
+        window:focus()
+
     else
-        if application then
-            print("HERE")
-            printTable(application:allWindows())
-            print(application:allWindows()[1]:role())
-            application:allWindows()[1]:becomeMain()
-            application:allWindows()[1]:maximize()
-            application:activate()
-            application:unhide()
-        else
-            M.marks[key].application = hs.application.open(application, 0, true)
-        end
+        -- In this scenario we were unable to recover the window from a previous state
+        -- so we just show whatever window is available for the application
+
+        application:unhide()
+        application:activate()
+    end
+
+    if M.marks[key].window ~= application:focusedWindow() then
+        -- If the window has been updated then we want to save it
 
         M.marks[key].window = application:focusedWindow()
         hs.json.write(getSerializedMarks(), MARKS_FILE_PATH, true, true)
@@ -97,13 +130,13 @@ hs.hotkey.bind({ "ctrl" }, "m", function()
                 application = focusedWindow:application(),
             }
             hs.notify.new(nil, {
-                informativeText = string.format("Mark('%s') = %s", event:getCharacters(true),
+                informativeText = string.format("Mark[%s] = %s", event:getCharacters(true),
                     focusedWindow:application():name()),
                 -- subTitle = focusedWindow:application():name(),
                 title = "Mark set",
                 withdrawAfter = 2
             }):send()
-            print(string.format("Mark('%s') = %s", event:getCharacters(true), focusedWindow:application():name()))
+            print(string.format("Mark[%s] = %s", event:getCharacters(true), focusedWindow:application():name()))
 
             hs.json.write(getSerializedMarks(), MARKS_FILE_PATH, true, true)
         end
@@ -128,9 +161,11 @@ hs.hotkey.bind({ "ctrl" }, "`", function()
             showApplication(application, window, key)
         end
 
+        -- Ensure event tap event only happens once
         M.events.gotoMark:stop()
         return true
     end)
+    -- Requires manually starting the event listener
     M.events.gotoMark:start()
 end)
 
@@ -146,7 +181,9 @@ end)
 chooser:rows(5)
 chooser:width(40)
 chooser:hideCallback(function()
+    -- Reset the search box
     chooser:query(nil)
+    -- Clear the develteMark keypress event
     M.events.deleteMark:stop()
 end)
 
