600c139feb181a860b919f42e9bf8ed1c8177237
Nathan Schott <nschott3@gmail.com>
2024-02-12 22:53:52 -0500

    Explicit swap option

--- scripts/tmux-nvim-remote.sh
+++ scripts/tmux-nvim-remote.sh
@@ -1,45 +1,61 @@
 #!/usr/bin/env bash
 
-function nv () {
+nv() {
+    local arguments=()
+
     for arg in "$@"; do
         # Check if contains a remote arg to determine using `nvim` vs `nvr`
-        if [[ $arg == *remote* ]]; then
-            is_remote="true"
-            break
+        if [[ $arg == --remote* ]]; then
+            local is_remote="true"
+        fi
+
+        if [[ $arg == --swap ]]; then
+            local should_swap="true"
+        else
+            arguments+=("$arg")
         fi
     done
 
-    if [[ $TMUX ]]; then
-        dir=$(getconf DARWIN_USER_TEMP_DIR)
+    open_tmux_nvim() {
+        local tmux_window=$(tmux display -p "#{session_id}.#{window_id}.#{pane_id}")
+        nvim --listen "${dir}nvim.$USER.$tmux_window" ${arguments[@]}
+        
+        return 0
+    } 
 
+    if [[ -z $TMUX ]]; then
         if [[ $is_remote == "true" ]]; then
-            # Search for first existing neovim instance in the current session
-            read -r -a tmux_ids <<< $(tmux list-panes -s -F '#{pane_current_command}:#{session_id} #{window_id} #{pane_id}' | awk -F ":" '/^nvim:/ {print $2}' | head -n1)
-
-            # If no instances exist
-            if [[ -n $tmux_ids ]]; then
-                session_id=${tmux_ids[0]}
-                window_id=${tmux_ids[1]}
-                pane_id=${tmux_ids[2]}
-
-                nvr --servername "${dir}nvim.$USER.$session_id.$window_id.$pane_id" $@
-
-                tmux select-window -t "$window_id"
-                tmux select-pane -t "$pane_id"
-
-                exit 0
-            fi
+            nvr ${arguments[@]}
+        else
+            nvim ${arguments[@]}
         fi
 
-        tmux_window=$(tmux display -p "#{session_id}.#{window_id}.#{pane_id}")
-        nvim --listen "${dir}nvim.$USER.$tmux_window" $@
-    else
-        if [[ $is_remote == "true" ]]; then
-            nvr $@
-            exit 0
-        fi
+        return 0
+    fi
 
-        nvim $@
+    if [[ $is_remote != "true" ]]; then
+        return open_tmux_nvim
+    fi
+
+    local dir=$(getconf DARWIN_USER_TEMP_DIR)
+    local session_id window_id pane_id
+
+    OLDIFS=$IFS
+    IFS=" "
+    # Search for first existing neovim instance in the current session
+    read -r session_id window_id pane_id <<< $(tmux list-panes -s -F '#{pane_current_command}:#{session_id} #{window_id} #{pane_id}' | awk -F ":" '/^nvim:/ {print $2}' | head -n1)
+    IFS=$OLDIFS
+
+    # If no instances exist
+    if [[ -z $pane_id ]]; then
+        return open_tmux_nvim
+    fi
+
+    nvr --servername "${dir}nvim.$USER.$session_id.$window_id.$pane_id" ${arguments[@]}
+
+    if [[ $should_swap == "true" ]]; then
+        tmux select-window -t "$window_id"
+        tmux select-pane -t "$pane_id"
     fi
 }
 
