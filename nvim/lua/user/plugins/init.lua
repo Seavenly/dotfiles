43c554e71b734e2fbcc0c3f52dab99ac32dc1511
Nathan Schott <nschott3@gmail.com>
2024-02-17 13:29:18 -0500

    Refactor structure of some plugins

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -1,35 +1,10 @@
--- Automatically install lazy.nvim package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Use a protected call so we don't error out on first use
-local status_ok, lazy = pcall(require, "lazy")
-if not status_ok then
-    return
-end
-
--- Install your plugins here
-lazy.setup({
-    -- My plugins here
+return {
+    -- Helper Utils
     "nvim-lua/popup.nvim",   -- An implementation of the Popup API from vim in Neovim
     "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+
     "numToStr/Comment.nvim", -- Easily comment stuff
-    "kyazdani42/nvim-web-devicons",
-    "kyazdani42/nvim-tree.lua",
-    { "akinsho/bufferline.nvim", branch = "main" },
     "moll/vim-bbye",
-    "nvim-lualine/lualine.nvim",
     { "akinsho/toggleterm.nvim", branch = "main" },
     "ahmedkhalf/project.nvim",
     "lewis6991/impatient.nvim",
@@ -38,13 +13,6 @@ lazy.setup({
     "antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
     "folke/which-key.nvim",
     "karb94/neoscroll.nvim",
-
-    -- Colorschemes
-    -- "lunarvim/colorschemes", -- A bunch of colorschemes you can try out
-    "lunarvim/darkplus.nvim",
-    "pineapplegiant/spaceduck",
-    "folke/tokyonight.nvim",
-    "rebelot/kanagawa.nvim",
 
     -- cmp plugins
     "hrsh7th/nvim-cmp",         -- The completion plugin
@@ -82,4 +50,4 @@ lazy.setup({
 
     -- Training
     "ThePrimeagen/vim-be-good",
-})
+}
---
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -0,0 +1,127 @@
+-- automatically close the tab/vim when nvim-tree is the last window in the tab
+vim.api.nvim_exec(
+    [[ autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif ]]
+    , false)
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- Anything deeper than this autofolds when buffer opens
+vim.opt.foldlevel = 20
+
+local function custom_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', 'l', api.node.open.edit, opts('Edit'))
+    vim.keymap.set('n', 'v', api.node.open.vertical, opts('Vertical Split'))
+    vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Node'))
+end
+
+return {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    opts = {
+        disable_netrw = false,
+        hijack_netrw = true,
+        open_on_tab = false,
+        hijack_cursor = false,
+        update_cwd = true,
+        on_attach = custom_on_attach,
+        hijack_directories = {
+            enable = true,
+            auto_open = true,
+        },
+        diagnostics = {
+            enable = true,
+            icons = {
+                hint = "",
+                info = "",
+                warning = "",
+                error = "",
+            },
+        },
+        update_focused_file = {
+            enable = true,
+            update_cwd = true,
+            ignore_list = {},
+        },
+        system_open = {
+            cmd = nil,
+            args = {},
+        },
+        filters = {
+            dotfiles = false,
+            custom = {},
+        },
+        actions = {
+            open_file = {
+                quit_on_open = true,
+                resize_window = true,
+                window_picker = {
+                    enable = true,
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                    exclude = {
+                        filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                        buftype = { "nofile", "terminal", "help" },
+                    },
+                },
+            }
+        },
+        git = {
+            enable = true,
+            ignore = false,
+            timeout = 500,
+        },
+        view = {
+            width = 40,
+            side = "right",
+            number = false,
+            relativenumber = false,
+        },
+        trash = {
+            cmd = "trash",
+            require_confirm = true,
+        },
+        renderer = {
+            highlight_git = true,
+            root_folder_modifier = ":t",
+            icons = {
+                show = {
+                    git = true,
+                    folder = true,
+                    file = true,
+                    folder_arrow = true,
+                },
+                glyphs = {
+                    default = "",
+                    symlink = "",
+                    git = {
+                        unstaged = "",
+                        staged = "S",
+                        unmerged = "",
+                        renamed = "➜",
+                        deleted = "",
+                        untracked = "U",
+                        ignored = "◌",
+                    },
+                    folder = {
+                        arrow_open = "󰁅",
+                        arrow_closed = "",
+                        default = "",
+                        open = "",
+                        empty = "",
+                        empty_open = "",
+                        symlink = "",
+                    },
+                }
+            }
+        }
+    }
+}
