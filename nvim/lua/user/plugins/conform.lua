6fd2bbd0bc8d0f748064a8fc0a37cf47dbef7f5e
Nathan Schott <nschott@paypal.com>
2024-03-25 08:56:39 -0400

    Update conform plugin

--- nvim/lua/user/plugins/conform.lua
+++ nvim/lua/user/plugins/conform.lua
@@ -1,9 +1,17 @@
 return {
     'stevearc/conform.nvim',
-    event = "VeryLazy",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     opts = {
         formatters_by_ft = {
-            javascript = { { "prettierd", "prettier" } }
+            javascript = { { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            css = { { "prettierd", "prettier" } },
+            scss = { { "prettierd", "prettier" } },
+            html = { { "prettierd", "prettier" } },
+            markdown = { { "prettierd", "prettier" } }
         },
         format_on_save = {
             -- These options will be passed to conform.format()
@@ -11,14 +19,12 @@ return {
             lsp_fallback = true,
         },
     },
-    init = function(_, opts)
-        local conform = require "conform"
-
-        conform.setup(opts)
-
+    init = function()
         -- Manual Format command
         vim.api.nvim_create_user_command("Format", function(args)
+            local conform = require "conform"
             local range = nil
+
             if args.count ~= -1 then
                 local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
                 range = {
@@ -26,6 +32,7 @@ return {
                     ["end"] = { args.line2, end_line:len() },
                 }
             end
+
             conform.format({ async = true, lsp_fallback = true, range = range })
         end, { range = true })
     end
