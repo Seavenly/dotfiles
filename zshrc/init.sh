8b13818467db2fe0ed1565af3a3ba3ed56afbba2
Nathan Schott <nschott@paypal.com>
2024-04-26 08:56:32 -0400

    Update zshrc init

--- zshrc/init.sh
+++ zshrc/init.sh
@@ -14,7 +14,20 @@ eval "$(fnm env --use-on-cd)"
 export PATH="$PATH:./node_modules/.bin"
 export PATH="${HOME}/.pyenv/shims:${PATH}"
 
+# Use homebrew version of ruby if installed
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
+if [ -d "/usr/local/opt/ruby/bin" ]; then
+  export PATH=/usr/local/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
+
 # Start tmux with a specific project session
 bindkey -s '^a' "~/.dotfiles/scripts/tmux-sessionizer.sh\n"
 
 source ~/.dotfiles/scripts/tmux-nvim-remote.sh
+
+alias serve="http-server . --ssl --cert ~/.ssh/localhost/localhost.cer.pem --key ~/.ssh/localhost/localhost.key.pem"
+alias hurlenv="source ~/dev/hurl/scripts/set-hurl-env.sh"
