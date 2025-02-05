544bb65daa45ccdbda4ce4aac4fcb48d00e864b1
Nathan Schott <nschott@paypal.com>
2025-02-05 17:45:23 -0500

    fix sessionizer outside of tmux

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
+    # specific project has been passed in to open
     selected=$1
+elif [[ -z $TMUX ]]; then
+    # executed outside of tmux when first connecting to session
+    selected=$(find ~/dev ~/dev/sandbox -name '.git' -mindepth 2 -maxdepth 2 -type d | xargs dirname | fzf --delimiter '/' --with-nth -2,-1) 
 else
+    # find projects using tmux popup inside tmux session
     find_result="${TMPDIR}session.txt"
 
     rm -f $find_result
@@ -24,6 +29,10 @@ fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected
+fi
+
+if [[ -z $TMUX ]]; then
+    tmux a
 fi
 
 tmux switch-client -t $selected_name
