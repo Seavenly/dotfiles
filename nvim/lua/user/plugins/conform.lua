ff97ab2bda7f082fbbf9a4e9d54070f0dd53e4f2
Nathan Schott <nschott@paypal.com>
2025-02-05 16:48:03 -0500

    update nvim conform

--- nvim/lua/user/plugins/conform.lua
+++ nvim/lua/user/plugins/conform.lua
@@ -4,21 +4,23 @@ return {
     cmd = { "ConformInfo" },
     opts = {
         formatters_by_ft = {
-            javascript = { { "prettierd", "prettier" } },
-            javascriptreact = { { "prettierd", "prettier" } },
-            typescript = { { "prettierd", "prettier" } },
-            typescriptreact = { { "prettierd", "prettier" } },
-            css = { { "prettierd", "prettier" } },
-            scss = { { "prettierd", "prettier" } },
-            html = { { "prettierd", "prettier" } },
-            markdown = { { "prettierd", "prettier" } },
-            json = { { "prettierd", "prettier" } }
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+            javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+            typescript = { "prettierd", "prettier", stop_after_first = true },
+            typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+            css = { "prettierd", "prettier", stop_after_first = true },
+            scss = { "prettierd", "prettier", stop_after_first = true },
+            html = { "prettierd", "prettier", stop_after_first = true },
+            markdown = { "prettierd", "prettier", stop_after_first = true },
+            json = { "prettierd", "prettier", stop_after_first = true }
         },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
+        format_on_save = function(bufnr)
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
         formatters = {
             prettierd = {
                 env = {
@@ -43,5 +45,11 @@ return {
 
             conform.format({ async = true, lsp_fallback = true, range = range })
         end, { range = true })
+
+        vim.api.nvim_create_user_command("WriteNoFormat", function(args)
+            vim.g.disable_autoformat = true
+            vim.cmd("w")
+            vim.g.disable_autoformat = false
+        end, { desc = "Write without formatting" })
     end
 }
